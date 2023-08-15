39022fcb67b11851781fa942f86b34c15131afd4
thecmdrunner <thecmdrunner@proton.me>
2023-08-15 16:45:48 +0530

    add  languages

---
+++ index.js
@@ -0,0 +1,13 @@
+// Define supported languages and their corresponding greetings
+const greetings = {
+  English: "Hello, World!",
+  French: "Bonjour, le Monde!",
+  Spanish: "¡Hola, Mundo!",
+  German: "Hallo, Welt!",
+  Italian: "Ciao, Mondo!",
+  Japanese: "こんにちは、世界！",
+  Chinese: "你好，世界！",
+  Russian: "Привет, мир!",
+  Korean: "안녕, 세상!",
+  Arabic: "مرحبًا بالعالم!",
+}
